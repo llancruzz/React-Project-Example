cd82f65ba592c8e0b6782e6dcd3169e0bb73db08
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-02-20 14:44:44 +0000

    fix: Create first function component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,12 @@
+// This gives evereything that react itself can do, including callling react.createElement
+// Note: This line is actually not required in the latest versions of React
+import React from "react";
+
+// Create function to return html
+function FunctionalGreeting(){
+    return <h1>Hello from React!</h1>
+}
+
+// To display it, you have to export the function and import that function
+// where you want to use, on that case in App.js
+export default FunctionalGreeting
